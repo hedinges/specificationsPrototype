17324f3cd31728ccbda23bc5ebee945a08c744df
hedinges <xcosyns@gmail.com>
2017-11-29 11:28:47 +0100

    controller fix

--- src/main/java/com/itea/controller/SpecificationController.java
+++ src/main/java/com/itea/controller/SpecificationController.java
@@ -6,15 +6,16 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import static java.lang.Boolean.TRUE;
 
-@Controller
+@RestController
 @EnableAutoConfiguration
 public class SpecificationController {
 
 
-    @RequestMapping("/greeting")
+    @RequestMapping("/specification")
     public Specification greeting(@RequestParam(value="name", defaultValue="World") String name) {
         return new Specification(SpecificationType.TEXT, "country.iso3", TRUE,"ZERTYRE");
     }
